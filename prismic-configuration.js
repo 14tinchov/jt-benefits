3e0f4b3f974fd64e08bd76deeb97ff2581cc67af
Martin Villalba <14tinchov@gmail.com>
2020-01-20 14:20:46 -0300

    - Added new templates and conect with prismic

---
+++ prismic-configuration.js
@@ -0,0 +1,4 @@
+module.exports = {
+  apiEndpoint: 'https://jt-try.prismic.io/graphql',
+  accessToken: "MC5YaURsT1JBQUFDSUFjdlZr.77-9WHLvv706QO-_ve-_vT8RX--_ve-_vXDvv70pAWU4eO-_vXDvv73vv70677-977-9Mm3vv71YMQ"
+};
